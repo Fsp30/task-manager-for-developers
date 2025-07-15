e86a987950ff6e3c5273dff1907e7f5c37ac6897
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-15 20:32:19 -0300

    correction path dir

--- taskhub/taskhub/settings.py
+++ taskhub/taskhub/settings.py
@@ -16,12 +16,12 @@ env = environ.Env(
         DEBUG=(bool, False)
 )
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
