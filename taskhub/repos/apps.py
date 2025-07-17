a2c3efc17cdbd4c51c7a5c36d03a7957284fd8a1
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-16 21:48:06 -0300

    set view test

--- taskhub/repos/apps.py
+++ taskhub/repos/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from ..taskhub.database.connect import connect
 
 class ReposConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'repos'
+
