7b30048105a4e045fce0d6ee671bf1b8dff8de00
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-21 21:58:16 -0300

    Import read mongo_connect

--- taskhub/repos/apps.py
+++ taskhub/repos/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
-from ..taskhub.database.connect import connect
+from taskhub.database.connect import mongo_connect
 
 class ReposConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'repos'
 
+    def ready(self):
+        mongo_connect()
