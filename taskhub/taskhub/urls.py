af5a4a86f540dba315dda96c2a9fa276906936cc
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-21 22:05:40 -0300

    update url for mongoengine use

--- taskhub/taskhub/urls.py
+++ taskhub/taskhub/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django_mongoengine.mongo_admin.sites import site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin2/', site.urls),
     path('auth/', include('social_django.urls', namespace='social')),
     path('', include('repos.urls'))
 ]
