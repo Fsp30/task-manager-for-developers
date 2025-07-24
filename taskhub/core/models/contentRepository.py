e9e499f83ff8a642bc51c10f66c4a33edf8e7a5a
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-23 22:59:29 -0300

    create model(document-mongo) ContentRepository

---
+++ taskhub/core/models/contentRepository.py
@@ -0,0 +1,23 @@
+import datetime
+from mongoengine import Document, StringField, DateTimeField, DictField, ReferenceField, ListField, BooleanField
+
+class ContentRepository(Document):
+    repositoryId = ReferenceField('Repository')
+    discussionId = StringField(required=True)
+    tag = DictField()
+    created_at = DateTimeField(default=datetime.datetime.utcnow)
+
+class Note(ContentRepository):
+    title = StringField(max_length=120, required=True)
+    author = ReferenceField('User')
+    text = StringField(max_length=120, required=True)
+    created_at = DateTimeField(default=datetime.datetime.utcnow)
+
+class Task(ContentRepository):
+    title = StringField(required=True)
+    author = ReferenceField('User')
+    resolversId = ListField(ReferenceField('User'))
+    description = StringField()
+    created_at = DateTimeField(default=datetime.datetime.utcnow)
+    completed = BooleanField()
+    tag = DictField()
