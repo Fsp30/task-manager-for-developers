77ab010512740bc530c5284b758938eadf1ec0a5
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-25 20:03:51 -0300

    create get_chat_messages function

--- taskhub/core/services/chat_redis_control.py
+++ taskhub/core/services/chat_redis_control.py
@@ -13,5 +13,11 @@ def save_message(chat_id:str, userId:str, message:str):
         }
         r.rpush(key, json.dumps(data))
         r.expire(key, 60 * 60 * 48)
+        get_chat_messages(chat_id)
 
+def get_chat_messages(chat_id:str):
+        key = f"chat:{chat_id}"
+        raw_messages = r.lrange(key, 0 , -1)
+        return [json.loads(message) for message in raw_messages]
 
+def 
