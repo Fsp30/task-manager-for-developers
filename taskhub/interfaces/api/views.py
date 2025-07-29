7266188d06714a0c44d52904e75d534beabf1d0b
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-29 13:17:38 -0300

    create template to test

--- taskhub/interfaces/api/views.py
+++ taskhub/interfaces/api/views.py
@@ -1,37 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from social_django.models import UserSocialAuth
-from ...core.models import Task
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.conf import settings
+from requests_oauthlib import OAuth2Session
 
-from django.views.decorators.http import require_POST
+def github_login(request):
+    github = OAuth2Session(
+        settings.GITHUB_CLIENT_ID,
+        redirect_uri=settings.GITHUB_CALLBACK_URL,
+        scope=['read:user', 'user:email']
+    )
+    auth_url, state = github.authorization_url('https://github.com/login/oauth/authorize')
+    request.session['oauth_state'] = state
+    return redirect(auth_url)
 
+def github_callback(request):
+    github = OAuth2Session(
+        settings.GITHUB_CLIENT_ID,
+        state=request.session.get('oauth_state'),
+        redirect_uri=settings.GITHUB_CALLBACK_URL
+    )
+    token = github.fetch_token(
+        'https://github.com/login/oauth/access_token',
+        client_secret=settings.GITHUB_CLIENT_SECRET,
+        code=request.GET.get('code')
+    )
+    user_data = github.get('https://api.github.com/user').json()
+    request.session['user_data'] = {
+        'login': user_data.get('login'),
+        'token': token.get('access_token')
+    }
+
+    return redirect('/')
 
 def home(request):
-    if request.user.is_authenticated:
-        try:
-            github_login = request.user.social_auth.get(provider='github')
-            token = github_login.extra_data['access_token']
-            username = github_login.extra_data['login']
-        except UserSocialAuth.DoesNotExist:
-            token = None
-            username = request.user.username
-        return HttpResponse(f"""
-            <h1>Bem-vindo, {username}!</h1>
-            <p>Token do GitHub: {token}</p>
-            <a href='/logout/'>
-                <button>Logout</button>
-            </a>
-        """)
-    else:
-        return HttpResponse("""
-            <h1>Test dev's</h1>
-            <a href='/auth/login/github/'>
-                <button>Login com GitHub</button>
-            </a>
-        """)
+    user_data = request.session.get('user_data')
+    return render(request, 'home.html', {'user_data': user_data})
 
-@require_POST
-def create_task(request):
-    title = request.POST.get('title', 'Default task')
-    Task(title=title).save()
-    return HttpResponse('success, create task')
+def logout_view(request):
+    request.session.flush()
+    return redirect('/')
