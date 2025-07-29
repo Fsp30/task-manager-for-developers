d1fe34b86f7618f85cda8afe9db34750a37909f9
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-29 18:14:40 -0300

    create JWT and refresh

--- taskhub/interfaces/api/urls.py
+++ taskhub/interfaces/api/urls.py
@@ -1,27 +1,26 @@
-"""
-URL configuration for taskhub project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django_mongoengine.mongo_admin.sites import site
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+from rest_framework.routers import DefaultRouter
+from taskhub.interfaces.api.views.task_views import TaskViewSet
+
+# router = DefaultRouter()
+# router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin2/', site.urls),
-    path('auth/login/', views.github_login, name='github_login'),
-    path('auth/callback/', views.github_callback, name='github_callback'),
+    # path('auth/login/', views.github_login, name='github_login'),
+    # path('auth/callback/', views.github_callback, name='github_callback'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/', include(router.urls)), 
 ]
+
+ 
