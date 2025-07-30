dd14d6c71d1bf4a195b8181840188489d6f98957
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-30 19:57:16 -0300

    increment base urls to auth, and correction path

--- taskhub/interfaces/api/urls.py
+++ taskhub/interfaces/api/urls.py
@@ -1,40 +1,40 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from django_mongoengine.mongo_admin.sites import site
-from . import views
+from django_mongoengine.mongo_admin.sites import site as mongo_admin_site
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
 from rest_framework.routers import DefaultRouter
-from taskhub.interfaces.api.views.task_views import TaskViewSet
-from taskhub.interfaces.api.views.note_views import NoteViewSet
-from taskhub.interfaces.api.views.enterprise_views import EnterpriseViewSet
-from taskhub.interfaces.api.views.repository_views import RepositoryViewSet
-from taskhub.interfaces.api.views.contentRepository_views import ContentRepositoryViewSet
-from taskhub.interfaces.api.views.repositoryPermission_views import RepositoryPermissionViewSet 
-from taskhub.interfaces.api.views.user_views import UserViewSet 
+from taskhub.interfaces.api.views import (
+    task_views,
+    note_views,
+    enterprise_views,
+    repository_views,
+    contentRepository_views,
+    repositoryPermission_views,
+    user_views
+)
+from . import views_main
 
 
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='task')
-router.register(r'notes', NoteViewSet, basename='notes')
-router.register(r'enterprise', EnterpriseViewSet, basename='enterprise')
-router.register(r'repository', RepositoryViewSet, basename='repository')
-router.register(r'content_repository', ContentRepositoryViewSet, basename='content_repository')
-router.register(r'repository_permission', RepositoryPermissionViewSet, basename='repository_permission')
-router.register(r'users', UserViewSet, basename='users')
-
+router.register(r'tasks', task_views.TaskViewSet, basename='task')
+router.register(r'notes', note_views.NoteViewSet, basename='notes')
+router.register(r'enterprise', enterprise_views.EnterpriseViewSet, basename='enterprise')
+router.register(r'repository', repository_views.RepositoryViewSet, basename='repository')
+router.register(r'content_repository', contentRepository_views.ContentRepositoryViewSet, basename='content_repository')
+router.register(r'repository_permission', repositoryPermission_views.RepositoryPermissionViewSet, basename='repository_permission')
+router.register(r'users', user_views.UserViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin2/', site.urls),
-    # path('auth/login/', views.github_login, name='github_login'),
-    # path('auth/callback/', views.github_callback, name='github_callback'),
+    path('mongo-admin/', mongo_admin_site.urls),
+    path('auth/login/', views_main.github_login, name='github_login'),
+    path('auth/callback/', views_main.github_callback, name='github_callback'),
+    path('auth/logout/', views_main.logout_view, name='logout'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(router.urls)), 
+    path('api/', include(router.urls)),
+    path('', views_main.home, name='home'),
 ]
-
- 
