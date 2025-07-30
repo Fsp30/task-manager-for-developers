1131e24358187f444b9fabb812504fa17a36d408
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-30 00:34:21 -0300

    export module

--- taskhub/core/models/__init__.py
+++ taskhub/core/models/__init__.py
@@ -1,2 +1,5 @@
 from .contentRepository import ContentRepository
-
+from .enterprise import Enterprise
+from .repository import Repository
+from .repositoryPermission import RepositoryPermission
+from .user import User
