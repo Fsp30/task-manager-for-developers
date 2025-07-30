4d1d1c7f3048084e10d4978a54b1919c3d422cf5
Fsp30 <depaulacostafilipe@gmail.com>
2025-07-30 00:32:57 -0300

    add id unique in class

--- taskhub/core/models/user.py
+++ taskhub/core/models/user.py
@@ -2,7 +2,7 @@ import datetime
 from mongoengine import Document, StringField, DateTimeField, ListField
 
 class User(Document):
-    gitId = StringField(required=True)
+    gitId = StringField(required=True, unique=True)
     email = StringField(required=True)
     userName = StringField(max_length=100)
     created_at = DateTimeField(default=datetime.datetime.utcnow)
