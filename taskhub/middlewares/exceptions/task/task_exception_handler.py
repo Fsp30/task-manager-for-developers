bfc73bab508158da12997853e6fc9a8a527a1c7c
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-09 23:15:09 -0300

    add new status code errors

--- taskhub/middlewares/exceptions/task/task_exception_handler.py
+++ taskhub/middlewares/exceptions/task/task_exception_handler.py
@@ -3,31 +3,49 @@ from taskhub.interfaces.api.constants.status_codes import ErrorsAppStatus
 from taskhub.middlewares.exceptions.base_exception_handler import CustomAppException
 
 class TaskAlreadyExists(CustomAppException):
-        status_code = ErrorsAppStatus.CONFLICT
-        default_detail = "Taks já existente"
-        default_code = "task_exists"
-        app_code = AppCode.TASK_CREATED_FAILED  
+    status_code = ErrorsAppStatus.CONFLICT
+    default_detail = "Task já existente"
+    default_code = "task_already_exists"
+    app_code = AppCode.TASK_ALREADY_EXISTS  
+
+class TaskNotFound(CustomAppException):
+    status_code = ErrorsAppStatus.NOT_FOUND
+    default_detail = "Task não encontrada"
+    default_code = "task_not_found"
+    app_code = AppCode.TASK_NOT_FOUND
 
 class TaskFailCreate(CustomAppException):
     status_code = ErrorsAppStatus.BAD_REQUEST
     default_detail = "Falha ao criar task"
-    default_code = "create_task_failed"
-    app_code = AppCode.TASK_CREATED_FAILED
+    default_code = "task_creation_failed"
+    app_code = AppCode.TASK_CREATE_FAILED 
 
 class TaskFailList(CustomAppException):
     status_code = ErrorsAppStatus.BAD_REQUEST
-    default_detail = "Falha ao buscar task"
-    default_code = "find_task_failed"
+    default_detail = "Falha ao listar tasks"
+    default_code = "task_list_failed"
     app_code = AppCode.TASK_LIST_FAILED
+
+class TaskFailDetail(CustomAppException):
+    status_code = ErrorsAppStatus.BAD_REQUEST
+    default_detail = "Falha ao obter detalhes da task"
+    default_code = "task_detail_failed"
+    app_code = AppCode.TASK_LIST_FAILED  
 
 class TaskFailUpdate(CustomAppException):
     status_code = ErrorsAppStatus.BAD_REQUEST
     default_detail = "Falha ao atualizar task"
-    default_code = "update_task_failed"
+    default_code = "task_update_failed"
     app_code = AppCode.TASK_UPDATE_FAILED
 
 class TaskFailDelete(CustomAppException):
     status_code = ErrorsAppStatus.BAD_REQUEST
     default_detail = "Falha ao deletar task"
-    default_code = "delete_task_failed"
+    default_code = "task_deletion_failed"
     app_code = AppCode.TASK_DELETE_FAILED
+
+class TaskPermissionDenied(CustomAppException):
+    status_code = ErrorsAppStatus.FORBIDDEN
+    default_detail = "Permissão negada para acessar a task"
+    default_code = "task_permission_denied"
+    app_code = AppCode.TASK_LIST_FAILED 
