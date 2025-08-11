b9c059d54c09e267edb8e03738b3fe66703a189f
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-11 20:58:22 -0300

    update model to best integration service

--- taskhub/core/models/repositoryPermission.py
+++ taskhub/core/models/repositoryPermission.py
@@ -1,4 +1,14 @@
-from mongoengine import Document, DictField
+from mongoengine import Document, DictField,ReferenceField,ListField
 
 class RepositoryPermission(Document):
-    repositoryPermissionId = DictField()
+    repositoryPermissionId = DictField(requred=True)
+    admin_repository = ReferenceField('Repository', required=True)
+    admin_users = ListField(ReferenceField('User'))
+    
+    meta = {
+        'collection': 'repository_permissions',
+        'indexes': [
+            'admin_repository',
+            {'fields': ['admin_repository', 'admin_users'], 'unique': True}
+        ]
+    }
