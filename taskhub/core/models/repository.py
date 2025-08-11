d8dbd9bdc6df5265ba6f2c44429959a38066ba42
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-11 20:27:44 -0300

    update model to best integration service

--- taskhub/core/models/repository.py
+++ taskhub/core/models/repository.py
@@ -3,8 +3,17 @@ from mongoengine import Document, StringField, DateTimeField, ReferenceField
 
 class Repository(Document):
     repository_id = StringField(required=True, unique=True)
-    admin_repository = ReferenceField('RepositoryPermission') 
+    admin_repository = ReferenceField('RepositoryPermission')
     creator_Id = ReferenceField('User', required=True)
     enterpriseId = ReferenceField('Enterprise')
     created_at = DateTimeField(default=datetime.datetime.utcnow)
     content_Id = ReferenceField('ContentRepository')
+
+    meta = {
+        'collection': 'repositories',
+        'indexes': [
+            'repository_id',
+            'creator_Id',
+            'enterpriseId'
+        ]
+    }
