aad0e941cd6f143de28ff61a15f7e092dfde83b6
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-13 21:26:53 -0300

    improving model for better flow

--- taskhub/core/models/enterprise.py
+++ taskhub/core/models/enterprise.py
@@ -2,9 +2,19 @@ import datetime
 from mongoengine import Document, StringField, DateTimeField, ReferenceField, ListField
 
 class Enterprise(Document):
-    Owner_Id = ReferenceField('User', required=True)
+    owner_Id = ReferenceField('User', required=True),
+    devs_enterprise = ListField(ReferenceField('User'))
     gitId_enterprise = StringField(max_length=100, unique=True)
-    enterpriseId = StringField(max_length=100, required=True)
-    nameEnterprise = StringField(max_length=100)
+    enterpriseId = StringField(max_length=100, required=True, unique=True)
+    nameEnterprise = StringField(max_length=100, required=True, unique=True)
     repositorys_Id = ListField(ReferenceField('Repository')) 
     created_at = DateTimeField(default=datetime.datetime.utcnow)
+
+    meta = {
+        'collection': 'enterprises', 
+        'indexes': [
+            'enterpriseId',
+            'nameEnterprise', 
+            {'fields': ['created_at'], 'expireAfterSeconds': 3600*24*365}  
+        ]
+    }
