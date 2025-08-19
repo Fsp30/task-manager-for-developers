88f9eca92019b7403a5958ff433cb07cf759ad16
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-18 21:59:27 -0300

    improving model Task to better flow Aplication

--- taskhub/core/models/contentRepository.py
+++ taskhub/core/models/contentRepository.py
@@ -4,12 +4,14 @@ from mongoengine import Document, StringField, DateTimeField, DictField, Referen
 class ContentRepository(Document):
     repositoryId = ReferenceField('Repository')
     contentId = StringField(required=True, unique=True)
-    admin_users = ReferenceField('RepositoryPermission')
+    admin_users = ListField(ReferenceField('RepositoryPermission'))
     working_tag = StringField()
     created_at = DateTimeField(default=datetime.datetime.utcnow)
     updated_at = DateTimeField(default=datetime.datetime.utcnow)
     chat_id = StringField(unique=True)
     content_type = StringField(required=True, default='content_repository')
+    notes = ListField(ReferenceField('Note'))
+    tasks = ListField(ReferenceField('Task'))
     meta = {
         'allow_inheritance': True,
         'indexes': ['repositoryId', 'created_at', 'content_type']
@@ -21,20 +23,36 @@ class Note(ContentRepository):
     text = StringField(max_length=5000, required=True)  
     tags = ListField(StringField(max_length=50))
     meta = {
-        'indexes': ['author', 'title']
+        'indexes': ['author', 'title'],
+        'content_type': 'note'
     }
 
 class Task(ContentRepository):
     taskId = StringField(required=True, unique=True)
+    link_content_id = ReferenceField('ContentRepository')  # Relação direta
     title = StringField(required=True, max_length=120)
     author = ReferenceField('User')
-    resolversId = ListField(ReferenceField('User'))
+    resolvers = ListField(ReferenceField('User'))
     description = StringField(max_length=5000)
     status = StringField(choices=('pending', 'in_progress', 'completed', 'cancelled'), default='pending')
     priority = StringField(choices=('low', 'medium', 'high'), default='medium')
     deadline = DateTimeField()
     tags = ListField(StringField(max_length=50))
-    task_tag = DictField()
+    created_task_at = DateTimeField(default=datetime.datetime.utcnow)
+    updated_task_at = DateTimeField(default=datetime.datetime.utcnow)
+    
     meta = {
-        'indexes': ['author', 'resolversId', 'status', 'priority', 'deadline']
+        'indexes': [
+            'author',
+            'resolvers',
+            'status',
+            'priority',
+            'deadline',
+            'link_content_id'  
+        ],
+        'content_type': 'task'
     }
+    
+    def save(self, *args, **kwargs):
+        self.updated_task_at = datetime.datetime.utcnow()
+        super().save(*args, **kwargs)
