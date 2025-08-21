0aa27d8abbd75c4f907cdcfa9fcd7f9973bf8bf0
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-21 00:03:16 -0300

    create exception to invalid email

--- taskhub/middlewares/exceptions/base_exception_handler.py
+++ taskhub/middlewares/exceptions/base_exception_handler.py
@@ -46,3 +46,9 @@ class InputExceededCharacterLimit(CustomAppException):
     default_detail = "Valor de input excede o permitido"
     default_code = "length_input_max_invalid"
     app_code = AppCode.INPUT_EXCEEDED_CHARACTER_LIMIT
+
+class InvalidEmailFormat(CustomAppException):
+    status_code = ErrorsAppStatus.BAD_REQUEST
+    default_detail = "Falha ao adicionar email "
+    default_code = "verification_email_no_match"
+    app_code = AppCode.USER_INVALID_EMAIL
