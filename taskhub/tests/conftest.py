4f7d90762f1e2f895eac670fa4f953be405c4b93
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-26 20:20:40 -0300

    config set mock

--- taskhub/tests/conftest.py
+++ taskhub/tests/conftest.py
@@ -1,17 +1,15 @@
 import pytest
 import mongomock
 import fakeredis
-from mongoengine import connect, disconnect
-from django.conf import settings
-
+from taskhub.core.services.user_service import create_user
+from mongoengine import connect, disconnect, get_db
 
 @pytest.fixture(scope="session", autouse=True)
 def mongo_test_connection():
-    """Conexão Mongo mockada para rodar os testes."""
-    disconnect()  # garante que não vai pegar a conexão real
+    disconnect()  
     connect(
         db="test_db",
-        host="mongodb://localhost",  # não conecta de fato
+        host="mongodb://localhost",  
         alias="default",
         uuidRepresentation="standard",
         mongo_client_class=mongomock.MongoClient,
@@ -19,20 +17,25 @@ def mongo_test_connection():
     yield
     disconnect()
 
+@pytest.fixture(autouse=True)
+def clear_mongo_collections():
+    db = get_db()
+    for collection in db.list_collection_names():
+        db[collection].delete_many({})
 
 @pytest.fixture(scope="session")
 def fake_redis():
-    """Redis fake para testes."""
     return fakeredis.FakeRedis(decode_responses=True)
-
 
 @pytest.fixture
 def redis_client(monkeypatch, fake_redis):
-    """Substitui redis real pelo fake no código."""
-    # se no seu código vc importa Redis assim:
-    # from redis import Redis
-    # então aqui fazemos o patch
     import redis
-
     monkeypatch.setattr(redis, "Redis", lambda *a, **kw: fake_redis)
     return fake_redis
+
+
+
+@pytest.fixture
+def default_user():
+    user = create_user("123", "filipe@example.com", "Filipe")
+    return user
