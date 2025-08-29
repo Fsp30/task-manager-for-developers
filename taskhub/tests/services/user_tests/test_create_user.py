69f3cfacf75ce17b6daa908468163467955e15a1
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-29 20:19:52 -0300

    create tests to (create_user)

--- taskhub/tests/services/user_tests/test_create_user.py
+++ taskhub/tests/services/user_tests/test_create_user.py
@@ -15,6 +15,7 @@ from taskhub.core.models import User
 
 class TestCreateUserService:
 
+        @pytest.mark.principal
         def test_create_user_successfully(self):
                 user = create_user("124", "test@example.com", "test_example")  
 
@@ -29,10 +30,12 @@ class TestCreateUserService:
                 assert saved_user is not None
                 assert saved_user.email == "test@example.com"
 
+        @pytest.mark.principal
         def test_create_user_duplicate(self, default_user):
                 with pytest.raises(UserAlreadyExists):
                         create_user("123", "filipe@example.com", "Filipe")
         
+        @pytest.mark.userName
         def test_create_user_without_name_uses_git_id(self):
 
                 user = create_user("789", "withoutName@example.com")
@@ -43,7 +46,58 @@ class TestCreateUserService:
         def test_create_user_duplicate_email(self, default_user):
                 with pytest.raises(UserAlreadyExists):
                         create_user("user2", "filipe@example.com", "User Two")
+        
+        def test_create_user_empty_git_id(self):
+                with pytest.raises(InputEmptyOrNone):
+                        create_user("", "test@example.com")
+                with pytest.raises(InputEmptyOrNone):
+                        create_user(" ", "test@example.com")
+                with pytest.raises(InputEmptyOrNone):
+                        create_user(None,  "test@example.com")
 
+        @pytest.mark.userName
+        def test_create_user_with_userName_exceeds_100_characters(self):
+                long_name = "A" * 101
+                with pytest.raises(InputExceededCharacterLimit):
+                        create_user("userTest", "test@example.com", long_name)
+        
+        @pytest.mark.userName
+        def test_create_user_with_userName_exactly_100_characters(self):
+                username_test = "A" * 100
+                user = create_user("userTest", "test@example.com", username_test)
+
+                assert user.userName == username_test
+
+
+        @pytest.mark.git_id
+        def test_create_user_with_whitespace_in_gitId(self):
+                with pytest.raises(InputEmptyOrNone):
+                        create_user(" ", "test@example.com")
+                
+                user = create_user(" test ", "test@example.com")
+                assert user.gitId == " test "
+                assert user.userName == " test "
+                
+        @pytest.mark.timestamp
+        def test_create_user_timestame_is_UTC(self):
+                user = create_user("test", "test@example.com")
+
+                assert user.created_at.tzinfo == UTC
+                assert user.updated_at.tzinfo == UTC
+
+                assert abs((user.created_at - user.updated_at).total_seconds()) < 1
+
+        @pytest.mark.fails
+        def test_create_user_unexpected_exception_during_query(self, mocker):
+                mocker.patch('taskhub.core.services.user_service.User.objects', 
+                        side_effect=Exception("Query error"))
+                
+                with pytest.raises(UserFailCreate) as exc_info:
+                        create_user("123", "test@example.com")
+                
+                assert "Failed to create user with GitHub ID '123'" in str(exc_info.value)
+                assert "Query error" in str(exc_info.value)
+                        
 
 
 
