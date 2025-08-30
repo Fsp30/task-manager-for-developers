9fe18580983387190558e5e369e9a2bf277ef9df
Fsp30 <depaulacostafilipe@gmail.com>
2025-08-30 19:28:17 -0300

    improving datetime fields with UTC, and set default method save

--- taskhub/core/models/repositoryPermission.py
+++ taskhub/core/models/repositoryPermission.py
@@ -1,9 +1,13 @@
-from mongoengine import Document, DictField,ReferenceField,ListField, StringField
+from mongoengine import Document, DictField,ReferenceField,ListField, StringField, DateTimeField
+import datetime
+from datetime import UTC
 
 class RepositoryPermission(Document):
     repositoryPermissionId = StringField(required=True, unique=True)
     admin_repository = ReferenceField('Repository', required=True)
     admin_users = ListField(ReferenceField('User'))
+    created_at = DateTimeField(default=lambda: datetime.datetime.now(datetime.UTC))
+    updated_at = DateTimeField(default=lambda: datetime.datetime.now(datetime.UTC))   
     
     meta = {
         'collection': 'repository_permissions',
@@ -12,3 +16,8 @@ class RepositoryPermission(Document):
             {'fields': ['admin_repository', 'admin_users'], 'unique': True}
         ]
     }
+
+    def save(self, *args, **kwargs) -> None:
+       
+        self.updated_at = datetime.datetime.now(datetime.UTC)
+        super().save(*args, **kwargs)
